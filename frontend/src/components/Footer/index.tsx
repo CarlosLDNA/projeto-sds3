b909278ffaae8959d24074ce0836fcc32d3b4f77
Carlos Alberto Gaspar <ca.gaspar@gmail.com>
2021-05-04 07:47:13 -0300

    Alterado link

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/CarlosLDNA" target="_blank" rel="noreferrer">Carlos Alberto Gaspar</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/carlos-alberto-gaspar" target="_blank" rel="noreferrer">Carlos Alberto Gaspar</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
