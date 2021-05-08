22a566c64a3e67765b11aa8cf45823a3db188259
CarlosLDNA <ca.gaspar@gmail.com>
2021-05-07 21:06:49 -0300

    retirado import

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -2,7 +2,6 @@ import axios from "axios";
 import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
-import { isTemplateExpression } from "typescript";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from 'utils/requests';
 
