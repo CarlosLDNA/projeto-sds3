bcecc38f1af67f584b66ffe4258b9da4b6603b22
CarlosLDNA <ca.gaspar@gmail.com>
2021-05-07 21:47:26 -0300

    teste url

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = 'https://sds3-carlos.herokuapp.com' ?? 'http://localhost:8080';
